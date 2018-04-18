f0f14a6ca8d6834f2f56508d96c105d84ddf3e98
FrozenTear7 <RainbowPablo7@gmail.com>
2018-04-18 18:29:05 +0200

    WIP ex1 from lab3

---
+++ lab3/ex1/script.js
@@ -0,0 +1,54 @@
+let timeFunInterval, timeFunTimeout, timeFunRequest
+let funIntervalHandler, funTimeoutHandler, funRequestHandler
+
+const slowOperationsOuch = () => {
+  for (let i = 0; i < 1000000; i++) {
+    let x = 3
+    x = 69 * i * 420 * 9
+  }
+}
+
+const funInterval = () => {
+  slowOperationsOuch()
+  const newDate = new Date()
+  timeFunInterval = newDate.getTime() - timeFunInterval
+  console.log('fun interval: ' + timeFunInterval + ' ms')
+  console.log('----------------------------------')
+  timeFunInterval = newDate.getTime()
+}
+
+const funTimeout = () => {
+  slowOperationsOuch()
+  const newDate = new Date()
+  timeFunTimeout = newDate.getTime() - timeFunTimeout
+  console.log('fun timeout: ' + timeFunTimeout + ' ms')
+  console.log('----------------------------------')
+  timeFunTimeout = newDate.getTime()
+  funTimeoutHandler = window.setTimeout(funTimeout, 1000)
+}
+
+const funRequest = () => {
+  slowOperationsOuch()
+  const newDate = new Date()
+  timeFunRequest = newDate.getTime() - timeFunRequest
+  console.log('fun request: ' + timeFunRequest + ' ms')
+  console.log('_________________________________')
+  timeFunRequest = newDate.getTime()
+  window.requestAnimationFrame(funRequest)
+}
+
+const launchFunctions = () => {
+  const newDate = new Date()
+  timeFunInterval = newDate.getTime()
+  timeFunRequest = newDate.getTime()
+  timeFunTimeout = newDate.getTime()
+
+  funIntervalHandler = setInterval(funInterval, 1000)
+  funTimeoutHandler = window.setTimeout(funTimeout, 1000)
+  window.requestAnimationFrame(funRequest)
+}
+
+const stopFunctions = () => {
+  clearInterval(funIntervalHandler)
+  window.clearTimeout(funTimeoutHandler)
+}
